25d11dc5471589ba2d66b509b141d8023f8a1792
André Militão <andremco1992@outlook.com>
2024-10-12 12:55:08 -0300

    order by dataRegistroCriacao descending

--- src/main/java/org/finance/repositories/SetorRepository.java
+++ src/main/java/org/finance/repositories/SetorRepository.java
@@ -2,6 +2,7 @@ package org.finance.repositories;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import io.quarkus.panache.common.Page;
+import io.quarkus.panache.common.Sort;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.finance.models.data.Setor;
 
@@ -13,7 +14,7 @@ public class SetorRepository implements PanacheRepository<Setor> {
         return find("descricao", descricao).firstResult();
     }
     public List<Setor> findSetoresPaged(Integer pagina, Integer tamanho){
-        return findAll().page(Page.of(pagina, tamanho)).list();
+        return findAll(Sort.by("dataRegistroCriacao").descending()).page(Page.of(pagina, tamanho)).list();
     }
 }
 
