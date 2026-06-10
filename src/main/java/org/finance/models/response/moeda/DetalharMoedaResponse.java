95e68e5c37e9d1e4ff8e0512d43c205818f773fc
André Militão <andremco1992@outlook.com>
2026-06-10 00:44:58 -0300

    feat: adiciona DetalharMoedaResponse com dados financeiros detalhados
    - Cria classe `DetalharMoedaResponse` estendendo `MoedaResponse`.
    - Inclui atributos como valores totais, porcentagens de carteira e quantidades faltantes.

---
+++ src/main/java/org/finance/models/response/moeda/DetalharMoedaResponse.java
@@ -0,0 +1,23 @@
+package org.finance.models.response.moeda;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Setter
+@SuperBuilder
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class DetalharMoedaResponse extends MoedaResponse{
+    private double valorTotalAtivo;
+    private double valorTotalCompras;
+    private double valorTotalVendas;
+    private double carteiraIdealPorcento;
+    private double carteiraTenhoPorcento;
+    private double quantoQueroTotal;
+    private double quantoFaltaTotal;
+    private int quantidadeQueFaltaTotal;
+}
